c0decaaaeee75c475540a5d44316c2808ab192a4
Tuomas Tuomitie <tuomas@tuomitie.com>
2018-01-25 17:47:34 +0200

    Week 1.2

---
+++ week1/osa1/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa osa={props.osa1} tehtavia={props.tehtavia1}/>
+            <Osa osa={props.osa2} tehtavia={props.tehtavia2}/>
+            <Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
+        </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <p>{props.osa} {props.tehtavia}</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <p>Yhteensä {props.yht} tehtävää</p>
+    )
+}
+
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = 'Reactin perusteet'
+    const tehtavia1 = 10
+    const osa2 = 'Tiedonvälitys propseilla'
+    const tehtavia2 = 7
+    const osa3 = 'Komponenttien tila'
+    const tehtavia3 = 14
+
+    return (
+        <div>
+            <Otsikko kurssi={kurssi}/>
+            <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3}/>
+            <Yhteensa yht={tehtavia1 + tehtavia2 + tehtavia3}/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
