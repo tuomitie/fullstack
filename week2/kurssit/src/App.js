898d9f491414c5fb832a8196c211a8e4434c0ad1
Tuomas Tuomitie <tuomas@tuomitie.com>
2018-01-31 22:06:44 +0200

    Week 2.5

--- week2/kurssit/src/App.js
+++ week2/kurssit/src/App.js
@@ -1,46 +1,51 @@
 import React from 'react'
+import Kurssi from './components/Kurssi'
 
 const App = () => {
-    const kurssi = {
-        nimi: 'Half Stack -sovelluskehitys',
-        osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10,
-                id: 1
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7,
-                id: 2
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14,
-                id: 3
-            }
-        ]
-    }
-
-    const Kurssi = ({ kurssi }) => {
-        return (
-            <div>
-                <h1>{kurssi.nimi}</h1>
-                <div>
-                    {kurssi.osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>)}
-                </div>
-                <p>Yhteensä {Yhteismaara} tehtävää</p>
-            </div>
-        )
-    }
-
-    const Yhteismaara = kurssi.osat.reduce(function(sum, osat) {
-        return (sum + osat.tehtavia)
-    }, 0)
+    const kurssit = [
+        {
+            nimi: 'Half Stack -sovelluskehitys',
+            id: 1,
+            osat: [
+                {
+                    nimi: 'Reactin perusteet',
+                    tehtavia: 10,
+                    id: 1
+                },
+                {
+                    nimi: 'Tiedonvälitys propseilla',
+                    tehtavia: 7,
+                    id: 2
+                },
+                {
+                    nimi: 'Komponenttien tila',
+                    tehtavia: 14,
+                    id: 3
+                }
+            ]
+        },
+        {
+            nimi: 'Node.js',
+            id: 2,
+            osat: [
+                {
+                    nimi: 'Routing',
+                    tehtavia: 3,
+                    id: 1
+                },
+                {
+                    nimi: 'Middlewaret',
+                    tehtavia: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Kurssi kurssi={kurssi} />
+            <h1>Opetusohjelma</h1>
+            {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi}/>)}
         </div>
     )
 }
--- week2/kurssit/src/components/Kurssi.js
+++ week2/kurssit/src/components/Kurssi.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
-const Kurssi = ({ kurssi }) => {
+const Kurssi = ({kurssi}) => {
     return (
         <div>
-            <h1>Muistiinpanot</h1>
-            <ul>
-                {Osa.map(note => <Osa key={kurssi.osat.id} osa={kurssi.osat.nimi} />)}
-            </ul>
+            <h2>{kurssi.nimi}</h2>
+            <div>
+                {kurssi.osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>)}
+            </div>
+            <p>Yhteensä {Yhteismaara(kurssi)} tehtävää</p>
         </div>
     )
 }
 
-const Osa = ({ osa }) => {
-    return (
-        <li>{osa}</li>
-    )
-}
+const Yhteismaara = kurssi => kurssi.osat.reduce(function (sum, osat) {
+    return (sum + osat.tehtavia)
+}, 0)
 
 export default Kurssi
